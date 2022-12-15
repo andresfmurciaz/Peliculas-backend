ee9ef4af9ac2f088b65cda54d6d8a6bc528d2e99
andresf.murciaz <andresf.murciaz@gmail.com>
2022-12-15 15:03:48 -0500

    validacion de contraseña del front con la contraseña encriptada guardada en la BD

--- controller/usuariosCtrl.js
+++ controller/usuariosCtrl.js
@@ -10,6 +10,7 @@ const usuarioListar=async(req,res) =>{
 
 }
 
+//guardar usuario
 
 const usuarioSave= async( req,res) =>{
     console.log(req.body)
@@ -26,45 +27,43 @@ res.status(200).json({msj:"usuario creado"})
     res.status(400).json({msj:"error creado"})
 }
 
-    //res.send("ok")
 }
 
 
-
-
+//login usuario
 
 const usuarioLogin=async(req,res)=>{
 console.log("intento de login")
 console.log(req.body)
-
-
-
+//captura dos variables que vienen del req
 const {correo,contrasena}= req.body
 try{
+
     errores=false
-    if(correo==""){errores=true
+    //valida si el correo y la contraseña que nos dan estan vacios
+    if(correo==""){
+        errores=true
         res.status(400).json({msj:"correo incorrecto"})
     }
-    if(contrasena==""){errores=true
+    if(contrasena==""){
+        errores=true
         res.status(400).json({msj:"contrasena incorrecto"})
     }
 if(!errores){
-//
-let usuario = await usuarioModel.find({'correo':correo,'contrasena':contrasena})
-console.log(usuario)
+//ME TRAE EL USUARIO DE LA BASE DE DATOS por el correo que nos manda el fronted
+let correcto = true
+let usuario = await usuarioModel.findOne({'correo':correo})
+console.log(usuario+ "usuario de BD")
+console.log(usuario.correo)
+correcto = await bcryptjs.compare(contrasena,usuario.contrasena)
 
-if(usuario==""){
-
+if(!correcto){
+    console.log("no encontrado usu")
     res.status(400).json({msj:"no"})
-
 }else{
-
-
-
+//jws nos permite crea un token para validad el lojin de los usuarios
 const payload={
-
 usuario:{
-
     id:usuario.id,
     nombre:usuario.nombre
 }   
@@ -74,27 +73,12 @@ usuario:{
     "palabrasecreta",
     (error,token)=>{
     if (error)throw error
-    res.status(200).json({msj:"ok acce so concedido", token:token})
+    res.status(200).json({msj:"ok", token:token})
 })
-
-
-
-
-
-
-
-
-
     //res.status(200).json({msj:"ok"})
-
 }
-
-
 }
-
 }catch(err){console.log("error usuaio")}
-
-
 
 }
 
